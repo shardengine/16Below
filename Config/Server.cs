3a22c90a2fd63da9ec6fd7faaa7e22075f28b580
jizzyjim <jameshirst89@live.co.uk>
2018-08-10 19:58:37 +0100

    1st Push
    As requested

--- Config/Server.cs
+++ Config/Server.cs
@@ -32,7 +32,7 @@ namespace Server.Configuration
 
         public class DataPath
         {
-            public static string    CustomPath          = "C:\\Ultima Online LBR";
+            public static string    CustomPath          = "C:\\Program Files\\Ultima Online 2D\\";
             public static bool      IgnoreStandardPaths = true;
         }
 
