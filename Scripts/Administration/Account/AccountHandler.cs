99e856afdfe5b379fa6f091d83052bc6e4bee243
jizzyjim <jameshirst89@live.co.uk>
2018-08-26 14:42:10 +0100

    Character Creation
    Advanced option - land in Felucca

--- Scripts/Administration/Account/AccountHandler.cs
+++ Scripts/Administration/Account/AccountHandler.cs
@@ -26,15 +26,15 @@ namespace Server.Misc
         public static CityInfo[] StartingCities = new CityInfo[]
         {
             //  new CityInfo("New Haven",   "New Haven Bank",           1150168, 3667,  2625,  0),
-                new CityInfo("Yew",         "The Empath Abbey",         1075072, 633,   858,   0),
-                new CityInfo("Minoc",       "The Barnacle",             1075073, 2476,  413,   15),
-                new CityInfo("Britain",     "The Wayfarer's Inn",       1075074, 1602,  1591,  20),
-                new CityInfo("Moonglow",    "The Scholars Inn",         1075075, 4408,  1168,  0),
-                new CityInfo("Trinsic",     "The Traveler's Inn",       1075076, 1845,  2745,  0),
-                new CityInfo("Magincia",    "The Great Horns Tavern",   1075077, 3738,  2223,  20),
-                new CityInfo("Jhelom",      "The Mercenary Inn",        1075078, 1374,  3826,  0),
-                new CityInfo("Skara Brae",  "The Falconer's Inn",       1075079, 618,   2234,  0),
-                new CityInfo("Vesper",      "The Ironwood Inn",         1075080, 2771,  976,   0)
+                new CityInfo("Yew",         "The Empath Abbey",         1075072, 633,   858,   0, Map.Felucca),
+                new CityInfo("Minoc",       "The Barnacle",             1075073, 2476,  413,   15, Map.Felucca),
+                new CityInfo("Britain",     "The Wayfarer's Inn",       1075074, 1602,  1591,  20, Map.Felucca),
+                new CityInfo("Moonglow",    "The Scholars Inn",         1075075, 4408,  1168,  0, Map.Felucca),
+                new CityInfo("Trinsic",     "The Traveler's Inn",       1075076, 1845,  2745,  0, Map.Felucca),
+                new CityInfo("Magincia",    "The Great Horns Tavern",   1075077, 3738,  2223,  20, Map.Felucca),
+                new CityInfo("Jhelom",      "The Mercenary Inn",        1075078, 1374,  3826,  0, Map.Felucca),
+                new CityInfo("Skara Brae",  "The Falconer's Inn",       1075079, 618,   2234,  0, Map.Felucca),
+                new CityInfo("Vesper",      "The Ironwood Inn",         1075080, 2771,  976,   0, Map.Felucca)
         };
 
 
